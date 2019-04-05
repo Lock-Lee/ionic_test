09aa2abb7b3e54d8af07654a1b59483f4e9a5bd2
Lock-Lee <locklee.on@email.com>
2019-04-05 14:56:23 +0700

    Add database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+export const firebaseConfig = {
+     apiKey: "AIzaSyAa3AHyI8eOYCbBX-5v12xNZeZF5pvFeMk",
+    authDomain: "test-ionic-ed2a0.firebaseapp.com",
+    databaseURL: "https://test-ionic-ed2a0.firebaseio.com",
+    projectId: "test-ionic-ed2a0",
+    storageBucket: "test-ionic-ed2a0.appspot.com",
+    messagingSenderId: "224895507517" 
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },AngularFireDatabase
   ],
   bootstrap: [AppComponent]
 })
